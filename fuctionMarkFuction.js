bcbd5bf2ee233ab0bfebfcf4f48465d841396d94
Mno <bourenmno1111@gmail.com>
2022-12-06 22:21:50 +0700

    add Oddeven

--- fuctionMarkFuction.js
+++ fuctionMarkFuction.js
@@ -1,12 +1,5 @@
 function hello(name){
 
-    return m => name + m ;
+
 
 } 
-
-const h = hello("mno ")
-const w = hello("nath")
-
-console.log(h("welcom "));
-console.log(h("hello "));
-
---
+++ oddEven.js
@@ -0,0 +1,7 @@
+function isEven (num){
+re=(num %2==0)? true : false;
+return re;
+    
+}
+console.log(isEven(50));
+console.log(isEven(75));
