f460586c1db28f0563c1d4208797209f46785e22
Mno <bourenmno1111@gmail.com>
2022-12-06 22:38:52 +0700

    OE?

--- oddEven.js
+++ oddEven.js
@@ -1,7 +1,6 @@
 function isEven (num){
-re=(num %2==0)? true : false;
-return re;
-    
+    return (num %2==0)? true : (num < 0 )? "???" : false ;
 }
 console.log(isEven(50));
 console.log(isEven(75));
+console.log(isEven(-1));
